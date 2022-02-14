306b87e88b295d54b86c4b9827495a51768dda03
Pedro <pedrolucasfsouza@gmail.com>
2022-02-14 15:36:09 -0300

    remove console

--- src/views/Header.ts
+++ src/views/Header.ts
@@ -1,36 +1,34 @@
-import { html, mounted } from '~/utils';
-import './Header.css';
-import { Player } from '~/models/Player';
-import albums from '~/mocks/albums.json';
+import { html, mounted } from "~/utils";
+import "./Header.css";
+import { Player } from "~/models/Player";
+import albums from "~/mocks/albums.json";
 
 export function Header() {
   mounted(function () {
     function playPauseMusic() {
       const containerPlayer = document
-        .getElementsByClassName('container-player')[0]
-        .getElementsByClassName('img-play-pause')[0];
+        .getElementsByClassName("container-player")[0]
+        .getElementsByClassName("img-play-pause")[0];
 
       if (!player.playing) {
         player.play();
         music!.play();
-        containerPlayer.setAttribute('src', '/img/pause.svg');
+        containerPlayer.setAttribute("src", "/img/pause.svg");
         setInterval(progressBar, 1000);
       } else {
         player.pause();
         music!.pause();
-        containerPlayer.setAttribute('src', '/img/play.svg');
+        containerPlayer.setAttribute("src", "/img/play.svg");
       }
-
-      console.log(player.playing);
     }
 
     function playPauseMusicNextPrev() {
       const containerPlayer = document
-        .getElementsByClassName('container-player')[0]
-        .getElementsByClassName('img-play-pause')[0];
+        .getElementsByClassName("container-player")[0]
+        .getElementsByClassName("img-play-pause")[0];
       player.play();
       music!.play();
-      containerPlayer.setAttribute('src', '/img/pause.svg');
+      containerPlayer.setAttribute("src", "/img/pause.svg");
       setInterval(progressBar, 1000);
     }
 
@@ -38,7 +36,7 @@ export function Header() {
       player.nextTrack();
 
       music?.setAttribute(
-        'src',
+        "src",
         `${albums[player._albumIndex].tracks[player._trackIndex].url}`
       );
       playPauseMusicNextPrev();
@@ -49,7 +47,7 @@ export function Header() {
     function voltarMusica() {
       player.prevTrack();
       music?.setAttribute(
-        'src',
+        "src",
         `${albums[player._albumIndex].tracks[player._trackIndex].url}`
       );
       playPauseMusicNextPrev();
@@ -58,24 +56,22 @@ export function Header() {
     }
 
     function progressBar() {
-      let progress = document.querySelector('progress');
+      let progress = document.querySelector("progress");
       progress?.setAttribute(`value`, `${music?.currentTime}`);
       progress?.setAttribute(`max`, `${music?.duration}`);
-
-      console.log(music?.currentTime);
     }
 
     function reRender() {
       for (let i = 0; albums.length > i; i++) {
         const containerItem = document.getElementsByClassName(
-          'container-album-musics'
+          "container-album-musics"
         )[i];
         // limpa as musicas
-        containerItem.innerHTML = '';
+        containerItem.innerHTML = "";
 
         // re renderiza os itens
         for (let j = 0; albums[i].tracks.length > j; j++) {
-          let divMusic = document.createElement('div');
+          let divMusic = document.createElement("div");
           containerItem.appendChild(divMusic);
 
           // muda a classe do CSS apenas caso seja a trackindex atual
@@ -90,17 +86,17 @@ export function Header() {
 
     // tirando junkiebox da caixa (instanciando a classe)
     const player = new Player();
-    const music = document.querySelector('audio');
+    const music = document.querySelector("audio");
 
     // configurando o junkiebox para sempre começar pela primeira música
     const musicStart = albums[0].tracks[0].url;
     music?.setAttribute(`src`, `${musicStart}`);
 
     // adicionando os albums (cds) que eu tenho no acervo, na "fila" do junkiebox
-    const containerAlbums = document.querySelector('.containeralbums');
+    const containerAlbums = document.querySelector(".containeralbums");
     for (let i = 0; albums.length > i; i++) {
       player.playlist.addAlbum(albums[i]);
-      let divalbum = document.createElement('div');
+      let divalbum = document.createElement("div");
       containerAlbums?.appendChild(divalbum);
       divalbum.innerHTML = `<div class="container-album"> <div class="content-album"> <img src="${player.playlist.albums[i].cover}" alt="artist" /> <div class="text-top-album"> <h1>${player.playlist.albums[i].title}</h1>  <h2>${player.playlist.albums[i].artist}</h2></div></div><div class="container-album-musics"></div></div>`;
     }
@@ -108,11 +104,11 @@ export function Header() {
     // adicionando as tracks (musicas) aos devidos albums
     for (let i = 0; albums.length > i; i++) {
       const containerItem = document.getElementsByClassName(
-        'container-album-musics'
+        "container-album-musics"
       )[i];
       playPauseMusic();
       for (let j = 0; albums[i].tracks.length > j; j++) {
-        let divMusic = document.createElement('div');
+        let divMusic = document.createElement("div");
         containerItem.appendChild(divMusic);
         // como default,
         if (j === 0 && i === 0) {
@@ -124,16 +120,16 @@ export function Header() {
 
     // Atribuindo as funções aos botões do player
     document
-      .querySelector('.img-play-pause')!
-      .addEventListener('click', playPauseMusic);
+      .querySelector(".img-play-pause")!
+      .addEventListener("click", playPauseMusic);
 
     document
-      .querySelector('.img-prev')!
-      .addEventListener('click', voltarMusica);
+      .querySelector(".img-prev")!
+      .addEventListener("click", voltarMusica);
 
     document
-      .querySelector('.img-next')!
-      .addEventListener('click', proximaMusica);
+      .querySelector(".img-next")!
+      .addEventListener("click", proximaMusica);
   });
 
   return html`
